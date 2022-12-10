750ed55ac79f7a402b3d70339314dc1ba797c269
Pranto Chakraborty <chakrabortypranto39@gmail.com>
2022-12-11 04:39:02 +0600

    success message close button issue solved

--- assets/mail/contact.js
+++ assets/mail/contact.js
@@ -26,7 +26,7 @@ $(function () {
                 cache: false,
                 success: function () {
                     $('#success').html("<div class='alert alert-success'>");
-                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                    $('#success > .alert-success').html("<button type='button' class='close' data-bs-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
                         .append("<strong>Your message has been sent. </strong>");
